11e64d3bd690472e6c46c38b5c4d29d9bd3f7dd3
Lex <lexmasta@gmail.com>
2016-11-01 18:12:40 -0400

    changed listener from .change to .click

--- app/assets/javascripts/conversion_calculation.js
+++ app/assets/javascripts/conversion_calculation.js
@@ -1,10 +1,9 @@
 
     function industrySelect(){
-        $("#industry_names").change(function(){
+        $("#industry_names").click(function(){
         industryId = '#' + $(this).val();
-        console.log(industryId);
-        $(industryId).removeClass("hidden");
-        // $(industryId).toggleClass("hidden");
+        // console.log(industryId);
+        $(industryId).toggleClass("hidden");
         $(industryId + '_view_input').keyup(function(e) {
           view_count = $(this).val();
           var view_count_number = Math.abs(view_count);
@@ -13,14 +12,12 @@
           var masViewsConversions = eval(view_count_number)*masConversionRate;
           $(industryId + '_mas_conversion_input').val(masViewsConversions);
 
-          var industryConversionRate = $("#industry_conversion_rate").val();
+          var industryConversionRate = $(industryId + "_industry_conversion_rate").val();
           var industryViewsConversions = eval(view_count_number)*industryConversionRate;
           $(industryId + '_industry_conversion_input').val(industryViewsConversions);
         });
       });
     }
-    // $("#cep").on("change", hi);
-    $("#industry_names").change("change", industrySelect);
 
     window.onload = industrySelect
 

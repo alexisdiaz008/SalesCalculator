af46ba4dfdea0cbd21ab256a22d439a246276085
Lex <lexmasta@gmail.com>
2016-12-06 01:31:27 -0500

    fixed some javascript functionality

--- app/assets/javascripts/conversion_calculation.js
+++ app/assets/javascripts/conversion_calculation.js
@@ -1,24 +1,36 @@
 function industrySelect(){
-
     $("#industry_names li").click(function(){
     industryId = '#'+ $(this).attr("class");
     console.log(industryId);
     $(industryId).toggleClass("hidden");
-    $(industryId + '_view_input').keyup(function(e) {
+  });
+}
+
+function inputCalculations(){
+  $("tbody tr th input").click(function(){
+    inputClass = '#'+ $(this).attr("class");
+    console.log(inputClass);
+    $(inputClass + '_view_input').keyup(function(e) {
+      console.log(this)
       view_count = $(this).val();
       var view_count_number = Math.abs(view_count);
       console.log(view_count);
       var masConversionRate = .13;
       var masViewsConversions = eval(view_count_number)*masConversionRate;
-      $(industryId + '_mas_conversion_input').val(masViewsConversions);
+      $(inputClass + '_mas_conversion_input').val(masViewsConversions);
 
-      var industryConversionRate = $(industryId + "_industry_conversion_rate").val();
+      var industryConversionRate = $(inputClass + "_industry_conversion_rate").val();
       var industryViewsConversions = eval(view_count_number)*industryConversionRate;
-      $(industryId + '_industry_conversion_input').val(industryViewsConversions);
+      $(inputClass + '_industry_conversion_input').val(industryViewsConversions);
     });
   });
 }
 
-window.onload = industrySelect
+function runScript(){
+  industrySelect();
+  inputCalculations();
+}
+
+window.onload = runScript
 
 
